eb7bd47ee2199f6ed4888aca8d3127d2307607c7
ByeongminChoi <DeveloperChoi90@gmail.com>
2021-02-22 22:47:19 +0900

    day20 of HackerRank

--- src/Solution.java
+++ src/Solution.java
@@ -13,6 +13,26 @@ public class Solution {
         for(int a_i=0; a_i < n; a_i++){
             a[a_i] = in.nextInt();
         }
+        int numberOfSwaps = 0;
         // Write Your Code Here
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n-1; j++){
+                if(a[j] > a[j+1]){
+                    int temp = 0;
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    numberOfSwaps++;
+                }
+            }
+            if(numberOfSwaps == 0){
+                break;
+            }
+        }
+        System.out.println("Array is sorted in " + numberOfSwaps + " swaps");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n-1]);
     }
+
+
 }
